4fe688e0867185bc6579a74098d359c6c333b123
xincheng <chujiwu2013@gmail.com>
2017-06-04 20:57:39 +0800

    20170604

--- xmlnodecomp/xmlcompare.py
+++ xmlnodecomp/xmlcompare.py
@@ -1,18 +1,24 @@
 import os
 from xml.etree import ElementTree
+from xml.etree.ElementTree import ElementTree
 
 
 class CompareResult(object):
     def __init__(self):
         pass
 
+def compare(mod_xml_path, ori_xml_path):
+    mod_xml = ElementTree.parse(mod_xml_path)
+    ori_xml = ElementTree.parse(ori_xml_path)
+    _compare_node(mod_xml, ori_xml)
 
-def compare(modxmlpath, orixmlpath=None):
-    modxml = ElementTree.parse(modxmlpath)
-    if orixmlpath:
-        orixml = ElementTree.parse(orixmlpath)
-    for node in modxml.getroot():
-        for child in list(node):
-            print(child.attrib["name"])
 
+def _compare_node(mod_xml: ElementTree, ori_xml: ElementTree):
+    if mod_xml.getroot() and ori_xml.getroot():
+        mod_node_list = list(mod_xml.getroot())
+        ori_node_list = list(ori_xml.getroot())
+        if mod_node_list and ori_node_list:
+            for mode_node in mod_node_list:
+                if not mode_node in ori_node_list:
+                    pass
     pass
